c5a442d6e9fc469f89bdef7a8a0e03710f12f32a
Zach Jones <zacharytjones1@gmail.com>
2022-12-10 15:25:43 -0800

    Initial enemy created, bullets need functionality.

---
+++ Assets/Targeting.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeting : MonoBehaviour
+{
+
+    public float reload = 2;
+    private float cooldown = 0;
+    public bool targetAcquired = false;
+
+    [SerializeField]
+    public GameObject bullet;
+    private Transform gun;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    [SerializeField]
+    private string targetName = "PlayerCapsule";
+    void Start()
+    {
+        gun = GetComponentInChildren<Transform>();
+    }
+
+    private void OnTriggerEnter(Collider source)
+    {
+        Debug.Log(source.gameObject.name);
+        
+        if(source.gameObject.name == targetName)
+        {
+            targetAcquired = true;
+            target = source.gameObject;
+            Attack(target);
+        }
+        //Instantiate(bullet, gun); // Might need to use world space
+    }
+
+    public void Attack(GameObject target)
+    {
+        // this is the logic that actually fires the gunnnnnn.
+        this.transform.LookAt(target.transform, this.transform.up);
+        Debug.Log("Bang");
+        GameObject bulletA = Instantiate(this.bullet, this.transform.position, this.transform.rotation) as GameObject; // Might need to use world space
+        // We need to spawn a thing in here
+    }
+
+
+
+    private void Update()
+    {
+        if (targetAcquired && cooldown < reload)
+        {
+            cooldown += Time.deltaTime;
+        }else if(targetAcquired && cooldown > reload)
+        {
+            cooldown = 0;
+            Attack(target);
+
+        }
+    }
+}
